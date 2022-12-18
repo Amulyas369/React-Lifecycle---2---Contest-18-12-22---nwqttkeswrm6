0092fba3a1695bc6d5bbbcd0c68236d1a3163bfe
Amulyas369 <amulyafood369@gmail.com>
2022-12-18 18:48:42 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React,{useState} from 'react'
+import React from 'react'
 
 import '../styles/App.css';
-import { useState } from 'react';
+import React from 'react';
 
 
 class App extends React.Component {
